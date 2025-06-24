1a51308be94095663d27ae44e411eefc5ba3a1c3
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-06-24 12:39:12 +0530

    Added comments

--- Sentiment-Based Product Rating System/app/app.py
+++ Sentiment-Based Product Rating System/app/app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request
 from src.inference import predict_sentiment
 
+# Initialize Flask application
 app = Flask(__name__)
 
+# Define the route for the home page
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    # Initialize default variables
     rating = None
     sentiment = None
     confidence = None
     review = ""
+
+    # Check if the form was submitted
     if request.method == 'POST':
         review = request.form['review']
         sentiment, confidence, rating = predict_sentiment(review)
