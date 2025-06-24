02ce5f289354250384b0175b069a22e741be6b27
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-06-24 12:45:35 +0530

    Updated documentation

--- Sentiment-Based Product Rating System/src/train_model.py
+++ Sentiment-Based Product Rating System/src/train_model.py
@@ -1,3 +1,4 @@
+# Import libraries
 import joblib
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -5,24 +6,31 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from data_preprocessing import load_and_clean_data, get_train_test_split
 
+# Trains the model and saves it to a file
 def train_and_save_model(data_path, model_save_path):
+
+    # Load and clean data from CSV file
     df = load_and_clean_data(data_path)
+
+    # Split cleaned data into training and testing sets
     X_train, X_test, y_train, y_test = get_train_test_split(df)
     
-    # Build a pipeline: TF-IDF vectorizer + Logistic Regression
+    # It converts text to TF-IDF features and train a logistic regression model
     pipeline = Pipeline([
         ('tfidf', TfidfVectorizer(max_features=10000, ngram_range=(1,2))),
         ('clf', LogisticRegression(max_iter=1000))
     ])
-    
+
+    # Train model using training data
     print("Training model...")
     pipeline.fit(X_train, y_train)
-    
+
+    # Predict on test sets
     print("Evaluating model...")
     y_pred = pipeline.predict(X_test)
-    print(classification_report(y_test, y_pred))
+    print(classification_report(y_test, y_pred))                      # Show precision, recall, f1-score...
     
-    # Save model
+    # Saves trained {vectorizer + model} to a file
     joblib.dump(pipeline, model_save_path)
     print(f"Model saved to {model_save_path}")
 
