d7c7d7486e9e8b276f3e6b0506398f05b05732d6
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-08-29 10:34:34 +0530

    Added comments

--- sentiment_product_rating/app/app.py
+++ sentiment_product_rating/app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 from src.inference import predict_sentiment
 
+# Creates a Flask web application
 app = Flask(__name__)
 
+# This is the main page where users can submit their reviews
 @app.route('/', methods=['GET', 'POST'])
 def home():
     rating = None
@@ -12,7 +14,10 @@ def home():
     if request.method == 'POST':
         review = request.form['review']
         sentiment, confidence, rating = predict_sentiment(review)
+
+    # Renders the HTML page, showing the results and the entered review
     return render_template('index.html', rating=rating, sentiment=sentiment, confidence=confidence, review=review)
 
+# Starts the Flask app when this script is run directly
 if __name__ == "__main__":
     app.run(debug=True)
